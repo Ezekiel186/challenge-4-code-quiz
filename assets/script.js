2b4f49bb0c3dd5b85b14ec109be8405e8392509a
Ezekiel <ezekieljamolin186@gmail.com>
2023-09-30 16:02:40 -0700

    working correct answers

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,14 @@
 var start = document.querySelector("#start")
 var timerEl = document.getElementById("timer")
 var question1 = document.getElementById("question-1-container")
+var question2 = document.getElementById("question-2-container")
+var question3 = document.getElementById("question-3-container")
+var question4 = document.getElementById("question-4-container")
+var end = document.getElementById("end")
+var correct1 = document.querySelector(".correct1")
+var correct2 = document.querySelector(".correct2")
+var correct3 = document.querySelector(".correct3")
+var correct4 = document.querySelector(".correct4")
 
 start.addEventListener("click", function() {
     timer()
@@ -24,10 +32,22 @@ function timer() {
     }, 1000);
 }
 
-function next() {
+correct1.addEventListener("click", function() {
+    question1.classList.add("hide")
+    question2.classList.remove("hide")
+});
 
-}
+correct2.addEventListener("click", function() {
+    question2.classList.add("hide")
+    question3.classList.remove("hide")
+});
 
-function selectAnswer() {
+correct3.addEventListener("click", function() {
+    question3.classList.add("hide")
+    question4.classList.remove("hide")
+});
 
-}
+correct4.addEventListener("click", function() {
+    question4.classList.add("hide")
+    end.classList.remove("hide")
+});
