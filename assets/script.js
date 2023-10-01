2ab3a05e7d02f46fa95edc660ce29afe8e690425
Ezekiel <ezekieljamolin186@gmail.com>
2023-09-30 18:50:42 -0700

    added wrong answers and time saved

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,10 @@ var correct1 = document.querySelector(".correct1")
 var correct2 = document.querySelector(".correct2")
 var correct3 = document.querySelector(".correct3")
 var correct4 = document.querySelector(".correct4")
-
+var timeInterval;
+var timeLeft;
+var wrong = document.querySelectorAll(".wrong")
+// console.log(wrong);
 start.addEventListener("click", function() {
     timer()
     start.classList.add("hide")
@@ -17,19 +20,22 @@ start.addEventListener("click", function() {
 });
 
 function timer() {
-    var timeLeft = 60;
-    var timeInterval = setInterval(function() {
-        if (timeLeft > 1) {
-            timerEl.textContent = timeLeft;
+    timeLeft = 60;
+    updateTime();
+    timeInterval = setInterval(function() {
+        if (timeLeft > 0) {
             timeLeft--;
-        } else if (timeLeft === 1) {
-            timerEl.textContent = timeLeft;
-            timeLeft--;
+            updateTime();
         } else {
             timerEl.textContent = "";
             clearInterval(timeInterval);
-        }
+            endscreen();
+        } 
     }, 1000);
+}
+
+function updateTime() {
+    timerEl.textContent = timeLeft;
 }
 
 correct1.addEventListener("click", function() {
@@ -50,4 +56,26 @@ correct3.addEventListener("click", function() {
 correct4.addEventListener("click", function() {
     question4.classList.add("hide")
     end.classList.remove("hide")
+    clearInterval(timeInterval);
 });
+
+
+
+// wrong.addEventListener("click", function() {
+//     timeLeft = timeLeft-5;
+// });
+
+for (var i=0; i < wrong.length; i++) {
+    wrong[i].addEventListener("click", function() {
+        timeLeft = timeLeft-5;
+        alert("Wrong Answer")
+    });
+}
+
+function endscreen() {
+    question1.classList.add("hide")
+    question2.classList.add("hide")
+    question3.classList.add("hide")
+    question4.classList.add("hide")
+    end.classList.remove("hide")
+}
