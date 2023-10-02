608bfd4b5920a413e8185c77931c93e40dae079b
Ezekiel <ezekieljamolin186@gmail.com>
2023-10-01 21:45:55 -0700

    feature changed:wrong answers go to next question

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ This was my 4th homework assignment for my bootcamp. In this project we were tol
 
 This is a fun mini-quiz to test your wits. Your score is stored locally and only shows your most recent score saved. The main goal is to answer as fast as you can and get the Highest time possible. If you choose the wrong answer you'll have time subtracted from your timer.
 
+## Website
+
+(./assets/website.png)
+
+https://ezekiel186.github.io/challenge-4-code-quiz/
+
 ## Credits
 
 Github:https://github.com/Ezekiel186 Linkedin:https://www.linkedin.com/in/ezekiel-jamolin-747150291/
--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,11 @@ var correct3 = document.querySelector(".correct3");
 var correct4 = document.querySelector(".correct4");
 var timeInterval;
 var timeLeft;
-var wrong = document.querySelectorAll(".wrong");
+// var wrong = document.querySelectorAll(".wrong");
+var wrong1 = document.querySelectorAll(".wrong1");
+var wrong2 = document.querySelectorAll(".wrong2");
+var wrong3 = document.querySelectorAll(".wrong3");
+var wrong4 = document.querySelectorAll(".wrong4");
 var submit = document.getElementById("submit");
 var initials = document.getElementById("initials");
 var popUP = document.getElementById("pop-up")
@@ -86,9 +90,23 @@ correct4.addEventListener("click", function() {
 //     timeLeft = timeLeft-5;
 // });
 
-for (var i=0; i < wrong.length; i++) {
-    wrong[i].addEventListener("click", function() {
+// for (var i=0; i < wrong.length; i++) {
+//     wrong[i].addEventListener("click", function() {
+//         timeLeft = timeLeft-5;
+//         popUP.classList.remove("hide")
+//         setTimeout(function() {
+//         popUP.classList.add("hide");
+//         }, 1000);
+//         // alert("Wrong Answer")
+
+//     });
+// }
+
+for (var i=0; i < wrong1.length; i++) {
+    wrong1[i].addEventListener("click", function() {
         timeLeft = timeLeft-5;
+        question1.classList.add("hide")
+        question2.classList.remove("hide")
         popUP.classList.remove("hide")
         setTimeout(function() {
         popUP.classList.add("hide");
@@ -97,6 +115,50 @@ for (var i=0; i < wrong.length; i++) {
 
     });
 }
+
+for (var i=0; i < wrong2.length; i++) {
+    wrong2[i].addEventListener("click", function() {
+        timeLeft = timeLeft-5;
+        question2.classList.add("hide")
+        question3.classList.remove("hide")
+        popUP.classList.remove("hide")
+        setTimeout(function() {
+        popUP.classList.add("hide");
+        }, 1000);
+        // alert("Wrong Answer")
+
+    });
+}
+
+for (var i=0; i < wrong3.length; i++) {
+    wrong3[i].addEventListener("click", function() {
+        timeLeft = timeLeft-5;
+        question3.classList.add("hide")
+        question4.classList.remove("hide")
+        popUP.classList.remove("hide")
+        setTimeout(function() {
+        popUP.classList.add("hide");
+        }, 1000);
+        // alert("Wrong Answer")
+
+    });
+}
+
+for (var i=0; i < wrong4.length; i++) {
+    wrong4[i].addEventListener("click", function() {
+        timeLeft = timeLeft-5;
+        question4.classList.add("hide")
+        end.classList.remove("hide")
+        popUP.classList.remove("hide")
+        setTimeout(function() {
+        popUP.classList.add("hide");
+        clearInterval(timeInterval);
+        }, 1000);
+        // alert("Wrong Answer")
+
+    });
+}
+
 
 function endscreen() {
     question1.classList.add("hide")
